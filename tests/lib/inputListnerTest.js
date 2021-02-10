cb394e1d2266f926b505444f8fe861b88e96cad8
Oday <odaymard88@gmail.com>
2021-02-10 17:30:38 +0100

    add validation method

--- tests/lib/inputListnerTest.js
+++ tests/lib/inputListnerTest.js
@@ -6,6 +6,8 @@ const { expect } = chai;
 const sinonChai = require('sinon-chai');
 
 const { readFromStd } = require('../../lib/inputListner');
+const { validateTheCoord } = require('../../lib/inputListner');
+
 const { ioHelper } = require('../../lib/inputListner');
 
 chai.use(sinonChai);
@@ -17,8 +19,17 @@ describe('inputListner', () => {
       readFromStd();
       expect(createInputListnerSpy).to.has.been.called;
     });
-    it('should accept numbers from the input', () => {
-      // TODO;
-    });
+   
   });
+  describe('validateTheCoord',() => {
+    it ('should not aceept string characters', () => {
+      const result = validateTheCoord('wrong lat','wrong lon')
+      expect(result).to.eql(false);
+    })
+    it ('should not aceept out of range coordinates', () => {
+      const result = validateTheCoord('-89','181')
+      expect(result).to.eql(false);
+    })
+  })
+
 });
